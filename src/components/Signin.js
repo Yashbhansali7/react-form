a166829c6c85507e67fa7e5d5c2db3e2a43c73e1
Yashbhansali7 <yashbhansali55@gmail.com>
2021-07-19 18:39:09 +0530

    commit

---
+++ src/components/Signin.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { TextField } from './TextField';
+import * as Yup from 'yup';
+
+export const Signin = () => {
+  const validate = Yup.object({    
+    email: Yup.string()
+      .email('Email is invalid')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 charaters')
+      .required('Password is required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Password must match')
+      .required('Confirm password is required'),
+  })
+  return (
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }}
+      validationSchema={validate}
+      onSubmit={values => {
+        console.log(values)
+      }}
+    >
+      {formik => (
+        <div>
+          <h1 className="my-4 font-weight-bold .display-4">Sign Up</h1>
+          <Form>
+            <TextField label="Email" name="email" type="email" />
+            <TextField label="password" name="password" type="password" />
+            <TextField label="Confirm Password" name="confirmPassword" type="password" />
+            <button className="btn btn-dark mt-3" type="submit">Register</button>
+            <div></div>
+            <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button>
+          </Form>
+        </div>
+      )}
+    </Formik>
+  )
+}
