f4b5652fe76fe9a394e277a7af6b3f03f559434d
Yashbhansali7 <yashbhansali55@gmail.com>
2021-07-19 18:38:42 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import './App.css';
+import rocketImg from './assets/rocket.png';
+import { Signup } from './components/Signup';
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-5">
+          <Signup />
+        </div>
+        <div className="col-md-7 my-auto">
+          <img className="img-fluid w-100" src={rocketImg} alt=""/>
+        </div>
+      </div>
     </div>
   );
 }
