452a7d40a004a1753ea34c48dd7cde37cd714cba
Yashbhansali7 <yashbhansali55@gmail.com>
2021-07-20 10:06:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import './App.css';
-import rocketImg from './assets/rocket.png';
-import { Signup } from './components/Signup';
+import './app.css';
+import { Signin } from './components/Signin';
 
 function App() {
   return (
     <div className="container mt-3">
       <div className="row">
         <div className="col-md-5">
-          <Signup />
-        </div>
-        <div className="col-md-7 my-auto">
-          <img className="img-fluid w-100" src={rocketImg} alt=""/>
-        </div>
+          <Signin />
+        </div>        
       </div>
     </div>
   );
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -10,17 +10,43 @@ export const Signin = () => {
       .required('Email is required'),
     password: Yup.string()
       .min(6, 'Password must be at least 6 charaters')
-      .required('Password is required'),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Password must match')
-      .required('Confirm password is required'),
+      .required('Password is required'),      
+    oldPassword: Yup.string()
+      .required('Old password is required'),
+    newPassword: Yup.string()
+     .notOneOf([Yup.ref('oldPassword'), null], 'Password should not be same')
+     .min(6, 'New Password must be at least 6 charaters')
+     .required('New password is required'),
   })
   return (
+    <div>
     <Formik
       initialValues={{
         email: '',
         password: '',
-        confirmPassword: ''
+      }}
+      validationSchema={validate}
+      onSubmit={value => {
+        console.log(value)
+      }}
+    >
+      {formik => (
+        <div>
+          <h1 className="my-4 font-weight-bold .display-4">Sign Up</h1>
+          <Form>
+            <TextField label="Email" name="email" type="email" />
+            <TextField label="password" name="password" type="password" />            
+            <button className="btn btn-dark mt-3" type="submit">Register</button>
+            <div></div>
+            <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button>
+          </Form>
+        </div>
+      )}
+    </Formik>
+    <Formik
+      initialPass={{
+        oldPassword: '',
+        newPassword: '',
       }}
       validationSchema={validate}
       onSubmit={values => {
@@ -29,17 +55,17 @@ export const Signin = () => {
     >
       {formik => (
         <div>
-          <h1 className="my-4 font-weight-bold .display-4">Sign Up</h1>
-          <Form>
-            <TextField label="Email" name="email" type="email" />
-            <TextField label="password" name="password" type="password" />
-            <TextField label="Confirm Password" name="confirmPassword" type="password" />
-            <button className="btn btn-dark mt-3" type="submit">Register</button>
+          <h1 className="my-4 font-weight-bold .display-4">Change Your Password</h1>          
+          <Form>            
+            <TextField label="Old password" name="oldPassword" type="password" />
+            <TextField label="New Password" name="newPassword" type="password" />
+            <button className="btn btn-dark mt-3" type="submit">Change</button>
             <div></div>
             <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button>
           </Form>
         </div>
       )}
     </Formik>
+    </div>
   )
 }
